5e349f1a4c581764102068031baa06346c657599
vagrant <vagrant@ironhack>
2013-11-17 18:32:12 +0000

    Initial Rails project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TripSorter::Application.config.secret_key_base = 'f8bfa77e05ccff605b19e9306d64cc11991f62336df39abf3889fce2e7cd5f8da356a8305214bb20fcaeca9bfaa02b94cd8df7d3f0f3e2b1acbaabcdb8a28f31'
